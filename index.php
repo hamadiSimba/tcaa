d0e0426eafcc1689eb592c5c212b3a9a9a067d83
hamadiSimba <63913859+hamadiSimba@users.noreply.github.com>
2023-08-11 08:31:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Testing Site</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="popup">
+                <h1>Welcome</h1>
+            </div>
+        </div>
+    </body>
+</html>
